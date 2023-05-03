39221b797274918db5289e7f5a4fb13846a3a646
bitz161 <bitzgarcia1996@gmail.com>
2023-05-03 18:29:50 -0400

    fix notification count and message features

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { NotifvaluesService } from 'src/app/services/notifvalues.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+
+  constructor(public status:NotifvaluesService){}
+ngOnInit(): void {}
+
+countNotification() {
+  return this.status.status.filter(element => element.notif_status === false).length
+}
 
 }
